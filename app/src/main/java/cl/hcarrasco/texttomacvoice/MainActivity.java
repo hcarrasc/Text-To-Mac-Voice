05876f426f7a1310fc59728eb37af2bcd2bb64d7
Hector Carrasco <mail@hcarrasco.cl>
2016-01-09 11:01:59 -0300

    Adding rate app option in menu

--- app/src/main/java/cl/hcarrasco/texttomacvoice/MainActivity.java
+++ app/src/main/java/cl/hcarrasco/texttomacvoice/MainActivity.java
@@ -279,6 +279,20 @@ public class MainActivity extends AppCompatActivity {
                 ipConfigView.setVisibility(View.INVISIBLE);
                 recentView.setVisibility(View.INVISIBLE);
                 return true;
+            case R.id.menu_rate_app:
+                Log.i("INFO", "Menu Rate app");
+                Uri uri = Uri.parse("market://details?id=" + context.getPackageName());
+                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                // To count with Play market backstack, After pressing back button,
+                // to taken back to our application, we need to add following flags to intent.
+                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                try {
+                    startActivity(goToMarket);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://play.google.com/store/apps/details?id=" + context.getPackageName())));
+                }
+                return true;
             case R.id.menu_item_share:
                 Intent sendIntent = new Intent();
                 sendIntent.setAction(Intent.ACTION_SEND);
